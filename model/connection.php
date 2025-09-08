c745197b6023f556eae898e4d5d0cf8e7d961f47
Mateus <mateus.stofel.brito@gmail.com>
2025-09-08 00:59:56 -0300

    Update connection.php

--- model/connection.php
+++ model/connection.php
@@ -2,11 +2,12 @@
 
 class Conection {
     public function connect() {
-      $con = new mysqli("sql300.infinityfree.com", "if0_39604736", "6i5mPuy7VbEP", "if0_39604736_test");
+      $con = new mysqli("mysql.railway.internal", "root", "AaKdPcRRYSeIkjwQVLgkIphdyULKRsJJ", "railway");
       if($con->connect_error) {
         die("Connection failed: " . $con->connect_error);
       }
       return $con;
     }
 }
+
 

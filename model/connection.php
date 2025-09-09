0921fd54a27079143b0155606521ddb61c0010e2
Mateus <mateus.stofel.brito@gmail.com>
2025-09-08 21:51:40 -0300

    Update connection.php

--- model/connection.php
+++ model/connection.php
@@ -2,12 +2,21 @@
 
 class Conection {
     public function connect() {
-      $con = new mysqli("mysql.railway.internal", "root", "AaKdPcRRYSeIkjwQVLgkIphdyULKRsJJ", "railway");
+      $con = new mysqli("mysql-cp7h.railway.internal", "root", "VVXATdKixIImPGMIOuXeuhkoaACtOgqY", "railway");
       if($con->connect_error) {
         die("Connection failed: " . $con->connect_error);
       }
       return $con;
     }
 }
+public __construct(
+    ?string $hostname = null,
+    ?string $username = null,
+    #[\SensitiveParameter] ?string $password = null,
+    ?string $database = null,
+    ?int $port = null,
+    ?string $socket = null
+)
+
 
 
